de9831ca716020d3303cdd5d99f293c12b2e2c61
Mohitsagar236 <149520913+Mohitsagar236@users.noreply.github.com>
2025-05-12 15:32:18 +0530

    Set up main application files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <h1>TaskMaster</h1>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-gray-900 mb-8">TaskMaster</h1>
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+      <Toaster position="bottom-right" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
